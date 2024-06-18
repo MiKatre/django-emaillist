657b37eae204485861580c441daa3c47b7039720
MiKatre <0mica4420@gmail.com>
2024-06-18 13:32:02 +0200

    Update name

--- emaillist/apps.py
+++ emaillist/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class EmaillistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_emaillist"
+    name = "emaillist"
     label = "emaillist"
